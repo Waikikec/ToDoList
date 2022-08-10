89efdcb5bd96462b9a1cc2f0351a8c4e8b781bd7
Martin Yankov <martin.d.yankov@gmail.com>
2022-08-10 16:06:34 +0300

    Styled Components and Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
 import TaskContext from './contexts/task-store';
 import useLocalStorage from './hooks/use-local-storage';
 import FocusScreen from './screens/FocusScreen';
 import ListScreen from './screens/ListScreen';
+import { colors, GlobalStyle } from './styles';
 import { Task } from './types';
+
+const Layout = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: 35px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  margin-bottom: 45px;
+`;
+
+const TabButton = styled(NavLink)`
+  align-items: center;
+  background: #000;
+  color: #fff;
+  display: flex;
+  height: 62px;
+  justify-content: center;
+  text-decoration: none;
+  width: 120px;
+
+  &:first-child {
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+  }
+
+  &:last-child {
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+  }
+
+  &.active {
+    background: ${colors.primary};
+    color: #000;
+  }
+`;
 
 function App() {
   const [tasks, setTasks] = useLocalStorage<Task[]>('tasks', []);
 
   return (
-    <BrowserRouter>
-      <TaskContext.Provider value={[tasks, setTasks]}>
-        <nav>
-          <NavLink to="/">List</NavLink> - <NavLink to="/focus">Focus</NavLink>
-        </nav>
-        <br></br>
-        <Routes>
-          <Route path="/" element={<ListScreen />}></Route>
-          <Route path="/focus" element={<FocusScreen />}></Route>
-        </Routes>
-      </TaskContext.Provider>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <TaskContext.Provider value={[tasks, setTasks]}>
+          <Layout>
+            <Nav>
+              <TabButton to="/">List</TabButton>
+              <TabButton to="/focus">Focus</TabButton>
+            </Nav>
+            <br></br>
+            <Routes>
+              <Route path="/" element={<ListScreen />}></Route>
+              <Route path="/focus" element={<FocusScreen />}></Route>
+            </Routes>
+          </Layout>
+        </TaskContext.Provider>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/styles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const colors = {
+  primary: '#FFC93F',
+};
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        background: #222;
+        color: #fff;
+    }
+`;
