ccd520eba9c2af1893ef846505807e1727d1f092
Martin Yankov <martin.d.yankov@gmail.com>
2022-08-10 14:51:55 +0300

    Build Data Managment Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { shuffle } from 'lodash';
-import { nanoid } from 'nanoid';
-import { useState } from 'react';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import TaskContext from './contexts/task-store';
 import useLocalStorage from './hooks/use-local-storage';
 import FocusScreen from './screens/FocusScreen';
 import ListScreen from './screens/ListScreen';
@@ -9,53 +7,19 @@ import { Task } from './types';
 
 function App() {
   const [tasks, setTasks] = useLocalStorage<Task[]>('tasks', []);
-  const [focusedTaskId, setFocusedTaskId] = useState<string | undefined>(
-    undefined
-  );
-
-  const addTask = (task: Pick<Task, 'label'>) => {
-    const id = nanoid();
-    setTasks((tasks) => [
-      ...tasks,
-      { id, label: task.label, isCompleted: false },
-    ]);
-    if (!focusedTaskId) setFocusedTaskId(id);
-  };
-
-  const updateTaskCompletion = (taskId: string, isCompleted: boolean) => {
-    setTasks((tasks) =>
-      tasks.map((task) => {
-        if (task.id === taskId) return { ...task, isCompleted };
-        return task;
-      })
-    );
-  };
-
-  const focusedTask = tasks.find((task) => task.id === focusedTaskId);
-
-  const shuffleFocusedTask = () => {
-    setFocusedTaskId(shuffle(tasks.filter((task) => !task.isCompleted))[0]?.id);
-  };
-
-  const tasksApi = {
-    addTask,
-    focusedTask,
-    tasks,
-    setTasks,
-    shuffleFocusedTask,
-    updateTaskCompletion,
-  };
 
   return (
     <BrowserRouter>
-      <nav>
-        <NavLink to="/">List</NavLink> - <NavLink to="/focus">Focus</NavLink>
-      </nav>
-      <br></br>
-      <Routes>
-        <Route path="/" element={<ListScreen {...tasksApi} />}></Route>
-        <Route path="/focus" element={<FocusScreen {...tasksApi} />}></Route>
-      </Routes>
+      <TaskContext.Provider value={[tasks, setTasks]}>
+        <nav>
+          <NavLink to="/">List</NavLink> - <NavLink to="/focus">Focus</NavLink>
+        </nav>
+        <br></br>
+        <Routes>
+          <Route path="/" element={<ListScreen />}></Route>
+          <Route path="/focus" element={<FocusScreen />}></Route>
+        </Routes>
+      </TaskContext.Provider>
     </BrowserRouter>
   );
 }
---
+++ src/hooks/use-task-store.ts
@@ -0,0 +1,55 @@
+import { shuffle } from 'lodash';
+import { nanoid } from 'nanoid';
+import { useContext, useEffect, useState } from 'react';
+import TaskContext from '../contexts/task-store';
+import { Task } from '../types';
+
+const useTaskStore = () => {
+  const [tasks, setTasks] = useContext(TaskContext);
+
+  const [focusedTaskId, setFocusedTaskId] = useState<string | undefined>(
+    tasks.filter((task) => !task.isCompleted)[0]?.id
+  );
+
+  const addTask = (task: Pick<Task, 'label'>) => {
+    const id = nanoid();
+    setTasks((tasks) => [
+      ...tasks,
+      { id, label: task.label, isCompleted: false },
+    ]);
+    if (!focusedTaskId) setFocusedTaskId(id);
+  };
+
+  const updateTaskCompletion = (taskId: string, isCompleted: boolean) => {
+    setTasks((tasks) =>
+      tasks.map((task) => {
+        if (task.id === taskId) return { ...task, isCompleted };
+        return task;
+      })
+    );
+  };
+
+  const focusedTask = tasks.find((task) => task.id === focusedTaskId);
+
+  useEffect(() => {
+    if (focusedTask?.isCompleted)
+      setFocusedTaskId(tasks.filter((task) => !task.isCompleted)[0]?.id);
+  }, [tasks, focusedTask]);
+
+  const shuffleFocusedTask = () => {
+    setFocusedTaskId(shuffle(tasks.filter((task) => !task.isCompleted))[0]?.id);
+  };
+
+  const api = {
+    addTask,
+    focusedTask,
+    tasks,
+    setTasks,
+    shuffleFocusedTask,
+    updateTaskCompletion,
+  };
+
+  return api;
+};
+
+export default useTaskStore;
